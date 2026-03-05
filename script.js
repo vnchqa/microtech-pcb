40f70baa0b8fde75dbd5e1ed5d905bd620e55b97
vnchqa <154042517+vnchqa@users.noreply.github.com>
2026-03-05 23:43:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
-<script>
-function calculatePrice(){
+function calculatePrice() {
+    let width = document.getElementById('width').value;
+    let height = document.getElementById('height').value;
+    let qty = document.getElementById('qty').value;
+    let layers = document.getElementById('layers').value;
 
-let width=document.getElementById('width').value
-let height=document.getElementById('height').value
-let qty=document.getElementById('qty').value
-let layers=document.getElementById('layers').value
+    if (!width || !height || !qty) { alert("Введите параметры платы"); return; }
 
-if(!width||!height||!qty){
-alert("Введите параметры платы")
-return
+    let area = width*height/100;
+    let base = area*4;
+    if(layers==2) base*=1.2;
+    if(layers==4) base*=1.6;
+    if(layers==6) base*=2;
+
+    let price = Math.round(base*qty);
+    document.getElementById('price').innerText = "≈ "+price+" ₽";
 }
-
-let area=(width*height)/100
-let base=area*5
-
-if(layers==2) base*=1.2
-if(layers==4) base*=1.6
-if(layers==6) base*=2
-
-let price=Math.round(base*qty)
-
-document.getElementById('price').innerText="≈ "+price+" ₽"
-}
-</script>
