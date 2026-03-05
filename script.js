aeefbee055e55ec3b50dd57ff0ea76c700dfc74d
vnchqa <154042517+vnchqa@users.noreply.github.com>
2026-03-05 21:21:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+<script>
+function calculatePrice(){
+
+let width=document.getElementById('width').value
+let height=document.getElementById('height').value
+let qty=document.getElementById('qty').value
+let layers=document.getElementById('layers').value
+
+if(!width||!height||!qty){
+alert("Введите параметры платы")
+return
+}
+
+let area=(width*height)/100
+let base=area*5
+
+if(layers==2) base*=1.2
+if(layers==4) base*=1.6
+if(layers==6) base*=2
+
+let price=Math.round(base*qty)
+
+document.getElementById('price').innerText="≈ "+price+" ₽"
+}
+</script>
