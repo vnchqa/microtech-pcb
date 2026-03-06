7633d1a8a394f5a23df7900f415efa9775df6cfe
vnchqa <154042517+vnchqa@users.noreply.github.com>
2026-03-06 10:20:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function calculatePrice() {
     if (!width || !height || !qty) { alert("Введите параметры платы"); return; }
 
     let area = width*height/100;
-    let base = area*4;
+    let base = area*5;
     if(layers==2) base*=1.2;
     if(layers==4) base*=1.6;
     if(layers==6) base*=2;
